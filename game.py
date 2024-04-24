c8c5b29f803a07920915e3d760a7e61df6cd163a
Matthew Berhalter <mberhalter01@gmail.com>
2024-04-23 22:40:29 -0400

    wrote boilerplate for grid padding. use numpy.pad

--- game.py
+++ game.py
@@ -3,12 +3,19 @@
 import numpy as np
 import sys
 
+
+"""
+use this for different border modes(dead, wrap, live, copy):
+https://numpy.org/doc/stable/reference/generated/numpy.pad.html
+"""
+
 if __name__ == "__main__":
     try:
         filename = sys.argv[1]
         num_gens = sys.argv[2]
+        mode = sys.argv[3]
     except IndexError:
-        raise SystemExit(f"Usage: {sys.argv[0]} <filename> <# of generations>")
+        raise SystemExit(f"Usage: {sys.argv[0]} <filename> <# of generations> <padding mode>")
     
 
     width = 80
@@ -26,5 +33,22 @@ if __name__ == "__main__":
         for col in row:
             sys.stdout.write(str(col))
         print()
+
+    match mode:
+        case "live":
+            pass
+        case "dead":
+            pass
+        case "wrap":
+            pass
+        case "copy":
+            pass
+        case _:
+            raise ValueError("Invalid border mode. Valid inputs: \n" + 
+                             "'live' - grid border is padded with live cells\n" +
+                             "'dead' - grid is padded with dead cells\n" +
+                             "'wrap' - grid is padded with cells from the opposite side\n" +
+                             "'copy' - grid is padded with a copy of the adjacent cell")
+
 
 
