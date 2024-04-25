001bdca51cec3af7419f573f9624624d4dfdd9fd
Matthew Berhalter <mberhalter01@gmail.com>
2024-04-24 20:12:51 -0400

    implemented game of life functionality. need to add further tests

--- game.py
+++ game.py
@@ -15,42 +15,45 @@ def print_grid(grid):
 
 
 
-def pad_grid(grid, mode):
-    """ Pad the border of a grid depending on the border mode (see below).
+def pad_grid(grid, pad_mode):
+    """ Pad the border of a grid depending on the padding mode (see below).
     Return the padded grid. 
 
     Valid modes:
      - 'live' counts cells outside of the grid as live.
      - 'dead' counts cells outside of the grid as dead.
      - 'wrap' counts cells outside of the grid as the value of the cells on the opposite side.
-     - 'copy' counts cells outside of the grid as the same value as the cells on that edge.
+     - 'symmetric' counts cells outside of the grid as the same value as the cells on that edge.
     """
-    match mode:
+    match pad_mode:
         case "live":
-            grid = np.pad(grid, 1, mode="constant", constant_values=1)
-            print("\nlive")
+            grid = np.pad(grid, 1, mode="constant", constant_values=live)
+            print("\nlive border:")
             print_grid(grid)
         case "dead":
-            grid = np.pad(grid, 1, mode="constant", constant_values=0)
-            print("\ndead")
+            grid = np.pad(grid, 1, mode="constant", constant_values=dead)
+            print("\ndead border:")
             print_grid(grid)
         case "wrap":
-            grid = np.pad(grid, 1, mode="wrap")
-            print("\nwrap")
+            grid = np.pad(grid, 1, mode=pad_mode)
+            print("\nwrap border:")
             print_grid(grid)
-        case "copy":
-            grid = np.pad(grid, 1, mode="symmetric")
-            print("\ncopy")
+        case "symmetric":
+            grid = np.pad(grid, 1, mode=pad_mode)
+            print("\nsymmetric border:")
             print_grid(grid)
         case _:
-            raise ValueError("Invalid border mode. Valid modes: live | dead | wrap | copy")
+            raise ValueError("Invalid padding mode. Valid modes: live | dead | wrap | symmetric")
+    return grid
 
 
-def count_neighbors(grid):
+def count_neighbors(grid, y, x):
     """Return the number of live neighbors for the cell at the given position in the grid."""
-    pass
+    return grid[y-1, x-1] + grid[y-1, x] + grid[y-1, x+1] + \
+           grid[y,   x-1]                + grid[y,   x+1] + \
+           grid[y+1, x-1] + grid[y+1, x] + grid[y+1, x+1]
 
-def compute_next_gen(grid):
+def compute_next_gen(grid, pad_mode):
     """Compute the next state for each non-border cell of the input grid based on its number of live neighbors.
     Return the newly calculated grid.
 
@@ -60,19 +63,42 @@ def compute_next_gen(grid):
     * Any live cell with more than three live neighbors dies, as if by overpopulation.
     * Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
     """
-    pass
+    retval = grid.copy()
+    #ranges need to exclude the first and last elements of each axis to avoid an indexing error.
+    for y in range(1, grid.shape[0]-1):
+        for x in range(1, grid.shape[1]-1):
+            neighbors = count_neighbors(grid, y, x)
+            if neighbors == 3:
+                retval[y, x] = live
+            elif grid[y, x] == live and neighbors == 2:
+                retval[y, x] = live
+            else:
+                retval[y, x] = dead
 
-def run_game(grid, num_gens):
+    #if the padding mode is either wrap or symmetric, we need to repad the grid
+    if pad_mode == "wrap" or pad_mode == "symmetric":
+        retval = np.pad(retval[1:-1, 1:-1], 1, mode=pad_mode)
+
+    return retval
+
+def run_game(grid, num_gens, pad_mode):
     """Plays game for given number of generations.
-    Prints output of each generation to terminal."""
-    pass
+    Prints output of each generation (without padding) to terminal."""
+    print("\ngeneration 0:")
+    print_grid(grid[1:-1, 1:-1])
+
+    for gen in range(1, num_gens+1):
+        print(f"\ngeneration {gen}:")
+        grid = compute_next_gen(grid, pad_mode)
+        print_grid(grid[1:-1, 1:-1])
+
 
 
 if __name__ == "__main__":
     try:
         filename = sys.argv[1]
-        num_gens = sys.argv[2]
-        mode = sys.argv[3]
+        num_gens = int(sys.argv[2])
+        pad_mode = sys.argv[3]
     except IndexError:
         raise SystemExit(f"Usage: {sys.argv[0]} <filename> <# of generations> <padding mode>")
     
@@ -80,4 +106,6 @@ if __name__ == "__main__":
     print("\nparsed input:")
     print_grid(grid)
 
-    grid = pad_grid(grid, mode)
+    grid = pad_grid(grid, pad_mode)
+
+    run_game(grid, num_gens, pad_mode)
