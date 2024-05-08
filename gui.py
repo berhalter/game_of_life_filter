5f07ed48cfb79ff9f9bae96994507f22a4e99749
Matthew Berhalter <mberhalter01@gmail.com>
2024-05-08 17:53:11 -0400

    created frame for image. still working on making image always centered in frame. switched from thorpy to pygame_gui

--- gui.py
+++ gui.py
@@ -1,56 +1,60 @@
-import pygame
-import random
-import thorpy as tp
+import pygame as pg
+import pygame_gui as gui
+
+
 
 # pygame setup
-pygame.init()
-bg_color = "lightslategray"
-screen_w, screen_h = 1200, 700
-screen = pygame.display.set_mode((screen_w, screen_h))
+pg.init()
+bg_color = "gray60"
+scr_size = pg.display.get_desktop_sizes()[0]
+scr_flags = pg.RESIZABLE 
+screen = pg.display.set_mode(scr_size, flags=scr_flags)
+image = pg.image.load("images/spongebob.jpg").convert()
+manager = gui.UIManager(scr_size)
 
 
-def my_stuff(): #do what you want with the display like in any pygame code you write
+hello_button = gui.elements.ui_selection_list.UISelectionList(
+                relative_rect=pg.Rect((350, 275), (100, 500)),
+                item_list=("a","b","C"),
+                allow_double_clicks=False,
+                manager=manager)
+
+
+def my_stuff(img): 
     screen.fill(bg_color)
+    size = pg.display.get_window_size()
+    offset = 25
 
-#Now let's pretend the UI elements below are what you need for your app:
-tp.init(screen, tp.theme_classic)
+    box = pg.Surface((size[0]-500, size[1]-2*offset))
+    box.fill("gray40")
+    box_rect = box.get_rect()
+    box_rect.move_ip(offset, offset)
 
-buttons1 = [tp.Button("Group1-"+str(i)) for i in range(10)]
-#give random pos for each button, since we did not sort them so far
-for b in buttons1:
-    b.set_topleft(x=random.randint(0,300), y=random.randint(100,screen_h-100))
-group1 = tp.Group(buttons1,None) #group elements without sorting them
+    img_scale = (box_rect.w-offset, box_rect.h-offset)
+    img = pg.transform.scale(img, img_scale)
+    img_rect = image.get_rect(center=box_rect.center)
+    img_rect.center = box_rect.center
 
-#group with vertically sorted elements
-group2 = tp.Group([tp.Button("Group2-"+str(i)) for i in range(10)], "v")
+    box.blit(img, img_rect)
+    screen.blit(box, box_rect)
 
-#group with horizontally sorted elements
-group3 = tp.Group([tp.Button("Group3-"+str(i)) for i in range(10)], "h")
-group3.stick_to(screen, "top", "top", delta=(0,5))
-
-#group with a grid of 2x5 elements.
-group4 = tp.Group([tp.Button("Group4-"+str(i)) for i in range(10)], None)
-group4.sort_children("grid", nx=2, ny=5)
-group4.stick_to(screen, "right", "right", delta=(-5,0))
-
-#group the groups.
-final_group = tp.Group([group1,group2,group3,group4], None)
-updater = final_group.get_updater() #this will be used to update the UI elements
 
 #Here is a very standard loop that includes only one line to update UI elements.
-clock = pygame.time.Clock()
-playing = True
-while playing:
-    clock.tick(60)
-    events = pygame.event.get()
-    for e in events:
-        if e.type == pygame.QUIT:
-            playing = False
-        else:
-            ... #do your stuff with events
-    my_stuff() #do your stuff with display
+clock = pg.time.Clock()
+running = True
+while running:
+    #clock.tick(60)
+    time_delta = clock.tick(60)/1000.0
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+        if event.type == gui.UI_BUTTON_PRESSED:
+            if event.ui_element == hello_button:
+                print('Hello World!')
+        manager.process_events(event)
 
-    #update Thorpy elements and draw them
-    updater.update(events=events)
-    pygame.display.flip()
-pygame.quit()
+    manager.update(time_delta)
+    my_stuff(image) #do your stuff with display
+    manager.draw_ui(screen)
+    pg.display.update()
+pg.quit()
